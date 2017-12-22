fe0aceb90e03f6dde1d2c32fff124579984b22a8
pujoljoaquin <pujoljoaquin@hotmail.com>
2017-12-22 16:17:41 -0300

    heroku

--- app/scripts/services/configuration.service.js
+++ app/scripts/services/configuration.service.js
@@ -2,7 +2,7 @@
 angular.module('iaw2017App')
     .service('Configuration', [ function () {
         var configurations = {
-            baseURL: "http://localhost:3000/api/v1"
+            baseURL: "https://iawbackend.herokuapp.com/api/v1"
         };
 
         this.getConfiguration = function() {
